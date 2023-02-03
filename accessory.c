65f8d2ac7a3eee5f625ba28036d4a93ff850f061
Travis Marks <travis@bmbx.net>
2023-02-02 18:14:30 -0700

    correct initial accessory.c

--- accessory.c
+++ accessory.c
@@ -13,22 +13,35 @@
 
 void accessory_identify(){};
 
-homekit_characteristic_t switcher = HOMEKIT_CHARACTERISTIC_(ON, false);
+homekit_characteristic_t switcher1 = HOMEKIT_CHARACTERISTIC_(ON, false);
+homekit_characteristic_t switcher2 = HOMEKIT_CHARACTERISTIC_(ON, false);
 
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]) {
         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
-            HOMEKIT_CHARACTERISTIC(NAME, "DoorbellChime"),
-            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "NinjaHome"),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "NHDBC0001B"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "DB8266"),
-            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+            HOMEKIT_CHARACTERISTIC(NAME, HK_ACC_NAME),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, HK_MFR),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, HK_SERIAL),
+            HOMEKIT_CHARACTERISTIC(MODEL, HK_MODEL),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, HK_FW),
             HOMEKIT_CHARACTERISTIC(IDENTIFY, accessory_identify),
             NULL
 		}),
         HOMEKIT_SERVICE(SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
-            &switcher,
-			HOMEKIT_CHARACTERISTIC(NAME, "DoorbellChime"),
+            &switcher1,
+			HOMEKIT_CHARACTERISTIC(NAME, "Outlet 1"),
+            NULL
+        }),
+        NULL
+    }),
+    HOMEKIT_ACCESSORY(.id=2, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Outlet 2"),
+            NULL
+		}),
+        HOMEKIT_SERVICE(SWITCH, .primary=false, .characteristics=(homekit_characteristic_t*[]) {
+            &switcher2,
+			HOMEKIT_CHARACTERISTIC(NAME, "Outlet 2"),
             NULL
         }),
         NULL
@@ -38,6 +51,6 @@ homekit_accessory_t *accessories[] = {
 
 homekit_server_config_t config = {
     .accessories = accessories,
-    .password = "111-11-111",
-    .setupId = "HKDB"
+    .password = HK_PWD,
+    .setupId = HK_SETUP
 };
